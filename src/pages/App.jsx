37f492278f1559e695addd8cb363c1166a2ed8ae
Sirvau <sirols50984@stud.noroff.no>
2024-10-25 21:57:49 +0200

    Created folders and files for upcoming pages

---
+++ src/pages/App.jsx
@@ -0,0 +1,14 @@
+import '../styles/App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hello, this is the main app "page"</h1>
+        <button className="bg-purple-200 hover:bg-purple-400 mt-4">I'm a pretty button</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
