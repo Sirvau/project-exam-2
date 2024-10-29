f200073e819cb6434d768eca210a842b71fde7ab
Sirvau <sirols50984@stud.noroff.no>
2024-10-29 14:21:08 +0100

    Created and config color and font-styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
+
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        base: [ 'Inria Sans',
+          'Inter',
+          'Avenir'],
+        header: ['Inria Serif', 'sans-serif'] },
+
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        modal: "var(--color-bg-modals)",
+        tBase:  "var(--color-text-base)",
+        overlayLight: "var(--color-overlay-light)",
+        overlayLighter:  "var(--color-overlay-lighter)",
+      }
+    }
   },
   plugins: [require('daisyui')]
 };
