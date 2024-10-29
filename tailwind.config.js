b13f3b308367717e1072475bef719b2724483e77
Sirvau <sirols50984@stud.noroff.no>
2024-10-29 21:39:16 +0100

    Code formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 
-
-
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        base: [ 'Inria Sans',
-          'Inter',
-          'Avenir'],
-        header: ['Inria Serif', 'sans-serif'] },
+        base: ['Inria Sans', 'Inter', 'Avenir'],
+        header: ['Inria Serif', 'sans-serif']
+      },
 
       colors: {
-        primary: "var(--color-primary)",
-        secondary: "var(--color-secondary)",
-        modal: "var(--color-bg-modals)",
-        tBase:  "var(--color-text-base)",
-        overlayLight: "var(--color-overlay-light)",
-        overlayLighter:  "var(--color-overlay-lighter)",
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        modal: 'var(--color-bg-modal)',
+        tBase: 'var(--color-text-base)',
+        overlayLight: 'var(--color-overlay-light)',
+        overlayLighter: 'var(--color-overlay-lighter)'
       }
     }
   },
