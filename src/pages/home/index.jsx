381ebc501300041e3a73bc7f68499c01dc36cdf3
Sirvau <sirols50984@stud.noroff.no>
2024-10-30 16:26:48 +0100

    Added sign-in-modal to homepage

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,11 @@
+import SignInModal from "../../modals/sign-in";
+
 function HomePage() {
+
   return (
     <div>
       <h1>Placeholder for homepage</h1>
+      <SignInModal/>
     </div>
   );
 }
