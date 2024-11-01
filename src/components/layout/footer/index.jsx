868fd9b0ff46e85d6fc8803478aa8aecfd1c44cb
Sirvau <sirols50984@stud.noroff.no>
2024-11-01 10:37:31 +0100

    Linting and formatting changes. Config. change to Eslint config file

--- src/components/layout/footer/index.jsx
+++ src/components/layout/footer/index.jsx
@@ -1,21 +1,21 @@
-import Logo from "../logo";
-import Copyright from "./copyright";
-import NavFooter from "./nav-footer";
-import SoMeIcons from "./social-media";
+import Logo from '../logo';
+import Copyright from './copyright';
+import NavFooter from './nav-footer';
+import SoMeIcons from './social-media';
 
 function Footer() {
   return (
-  <footer className="sticky footer footer-center">
-        <hr className="mt-12 border-overlayLighter w-full"></hr>
-  <aside>
-    <Logo/>
-    <NavFooter/>
-  </aside>
-    <div className="grid grid-flow-col gap-4">
-    <SoMeIcons/>
-    </div>
-    <Copyright/>
-</footer>
+    <footer className="sticky footer footer-center">
+      <hr className="mt-12 border-overlayLighter w-full"></hr>
+      <aside>
+        <Logo />
+        <NavFooter />
+      </aside>
+      <div className="grid grid-flow-col gap-4">
+        <SoMeIcons />
+      </div>
+      <Copyright />
+    </footer>
   );
 }
 export default Footer;
--- src/components/layout/footer/nav-footer/index.jsx
+++ src/components/layout/footer/nav-footer/index.jsx
@@ -1,16 +1,20 @@
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 
 function NavFooter() {
-    return (
+  return (
     <nav>
-        <ul>
-        <NavLink to="/"><li className="my-3 hover:scale-105 duration-300">Explore Venues</li></NavLink>
-        <NavLink to="/admin-page"><li className="my-3 hover:scale-105 duration-300">Admin Venues</li></NavLink>
-        <NavLink to="/profile-page"><li className="my-3 hover:scale-105 duration-300">Profile</li></NavLink>
+      <ul>
+        <NavLink to="/">
+          <li className="my-3 hover:scale-105 duration-300">Explore Venues</li>
+        </NavLink>
+        <NavLink to="/admin-page">
+          <li className="my-3 hover:scale-105 duration-300">Admin Venues</li>
+        </NavLink>
+        <NavLink to="/profile-page">
+          <li className="my-3 hover:scale-105 duration-300">Profile</li>
+        </NavLink>
       </ul>
     </nav>
-      
-    );
-  }
-  export default NavFooter;
-  
+  );
+}
+export default NavFooter;
