e7c24d7fde853d2573ac22871be18d80152e759b
Sirvau <sirols50984@stud.noroff.no>
2024-11-06 08:47:05 +0100

    Added functionalities for loading and removing keys from local storage

--- src/stores/local-storage/index.jsx
+++ src/stores/local-storage/index.jsx
@@ -1,3 +1,9 @@
 export const saveToStorage = (key, value) => {
   localStorage.setItem(key, JSON.stringify(value));
 };
+
+export function load(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
+
+export const remove = (key) => localStorage.removeItem(key);
