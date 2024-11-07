6a198eb6f00e6fca1c2107434fdef868afb97226
Sirvau <sirols50984@stud.noroff.no>
2024-11-07 19:20:52 +0100

    Created user details function from userStore. Loading user data into UserProfile section

---
+++ src/components/profile/profile-section/index.jsx
@@ -0,0 +1,48 @@
+import { useEffect } from 'react';
+import { useUserStore } from '../../../stores/user-store';
+import UserDetails from '../profile-info';
+import SubtileButton from '../../buttons/subtile-button';
+import { editProfileIcon } from '../../icons';
+
+function UserProfile() {
+  const { userData, loadUserData } = useUserStore();
+
+  useEffect(() => {
+    loadUserData();
+  }, [loadUserData]);
+
+  if (!userData) return <p>Loading user data...</p>;
+
+  const {
+    name = 'No name available',
+    email = 'No email available',
+    avatar = {},
+    banner = {},
+    bio = 'No bio available',
+    venueManager = false
+  } = userData?.data || {};
+
+  return (
+    <div className="relative bg-overlayLight rounded-b-md">
+      <UserDetails
+        userName={name}
+        userEmail={email}
+        userAvatar={avatar.url || 'default-avatar-url'}
+        avatarAlt={avatar.alt || 'No avatar description'}
+        userBanner={banner.url || 'default-banner-url'}
+        bannerAlt={banner.alt || 'No banner description'}
+        bio={bio}
+        venueManager={venueManager}
+      />
+      <div className="flex justify-center">
+        <SubtileButton
+          buttonText="Edit Profile"
+          icon={editProfileIcon}
+          onClick={() => document.getElementById('edit-profile-modal').showModal()}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default UserProfile;
