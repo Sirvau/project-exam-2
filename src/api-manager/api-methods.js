60d253ad485af21a6458d7829ac96ea860aa662b
Sirvau <sirols50984@stud.noroff.no>
2024-11-22 11:40:52 +0100

    Import headers to api-methods

--- src/api-manager/api-methods.js
+++ src/api-manager/api-methods.js
@@ -1,28 +1,4 @@
-
-import { loadFromStorage } from '../stores/local-storage';
-import useUserStore from '../stores/user-store';
-import { API_KEY } from '../constants';
-
-
-export const getHeaders = () => {
-    const accessToken = useUserStore.getState().accessToken ||  loadFromStorage('accessToken'); 
-    const apiKey = API_KEY;  
-  
-    const headers = {
-      'Content-Type': 'application/json',
-      'X-Noroff-API-Key': apiKey,  
-    };
-  
-    if (accessToken) {
-      headers['Authorization'] = `Bearer ${accessToken}`; 
-    } else {
-      console.warn('Access token not found!');
-    }
-  
-    return headers;
-  };
-  
-  
+import { getHeaders } from "./headers";
   
   
   class ApiMethods {
