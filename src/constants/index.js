b813337c653f6fda6e8ab2d594585d363ef71c1d
Sirvau <sirols50984@stud.noroff.no>
2024-12-18 13:04:03 +0100

    Relocated endpoints from env-file to contants. Removed unused code

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,19 +1,11 @@
-
-
-export const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-const LOGIN = import.meta.env.VITE_API_LOGIN;
-const REGISTER = import.meta.env.VITE_API_REGISTER;
-const ALL_VENUES = import.meta.env.VITE_API_ALL_VENUES;
-const EDIT_PROFILE = import.meta.env.VITE_API_EDIT_PROFILE;
-const SINGLE_PROFILE = import.meta.env.VITE_API_SINGLE_PROFILE;
-const CREATE_BOOKING = import.meta.env.VITE_API_CREATE_BOOKING;
-
 export const API_KEY = import.meta.env.VITE_API_KEY;
-export const LOGIN_URL = `${BASE_URL}${LOGIN}`;
-export const REGISTER_URL = `${BASE_URL}${REGISTER}`;
-export const ALL_VENUES_URL = `${BASE_URL}${ALL_VENUES}`;
-export const EDIT_PROFILE_URL = `${BASE_URL}${EDIT_PROFILE}`;
-
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const LOGIN = '/auth/login';
+const REGISTER = '/auth/register';
+const ALL_VENUES = '/holidaze/venues';
+const EDIT_PROFILE = '/holidaze/profiles';
+const SINGLE_PROFILE = '/holidaze/profiles';
+const CREATE_BOOKING = '/holidaze/bookings';
 
 
 
